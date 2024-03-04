7c55363a864376afd4926aa62c386c90dc0a3488
anissalassek <89753159+anissalassek@users.noreply.github.com>
2024-03-04 13:25:44 -0800

    added editable tag

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function() {
     $(".hamburger-button").click(function() {
         $(this).toggleClass("active");
@@ -57,3 +59,42 @@ for (i = 0; i < dropdown.length; i++) {
     }
   });
 }
+
+
+
+$('.testimon-slider-container').slick({
+  autoplay: true,        // Do we want it to autoplay? true or false
+	autoplaySpeed: 3000,   // How long between each slide when autoplaying
+	speed: 500,            // How fast is the transition 
+  arrows: false,          // Do you want to show arrows to trigger each slide
+  accessibility: true,   // Enables tabbing and arrow key navigation 
+  dots: true,            // Enables the dots below to show how many slides
+  fade: false,           // Changes the animate from slide to fade if true
+  infinite: true,       // When true, means that it will scroll in a circle
+  pauseOnHover: true,   // When true means the autoplay pauses when hovering
+  pauseOnDotsHover: true // Pauses the autoplay when hovering over the dots
+});
+
+$(".testimonial-slider-container").slick({
+    autoplay: true,
+    autoplaySpeed: 2e3,
+    speed: 600,
+    arrows: true,
+    accessibility: true,
+    dots: true,
+    fade: false,
+    infinite: false,
+    pauseOnHover: false,
+    pauseOnDotsHover: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [{
+        breakpoint: 768,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+        }
+    }]
+});
